3cf3c17c8cdc8ea0e071bd373d14c04d0944fcde
feeldacy <husnafelisacahyani@gmail.com>
2026-06-07 16:55:22 +0700

    add: base crafting mechanic

--- Assets/mangkok.gd
+++ Assets/mangkok.gd
@@ -1,12 +1,4 @@
 extends Node3D
 
-@export var ingredient_offset := Vector3.ZERO
-
-func add_ingredient(ingredient_scene):
-	var ingredient = ingredient_scene.instantiate()
-
-	add_child(ingredient)
-
-	ingredient.position = ingredient_offset
-
-	ingredient_offset.y += 0.05
+func add_ingredient():
+	print("Ingredient added to bowl")
